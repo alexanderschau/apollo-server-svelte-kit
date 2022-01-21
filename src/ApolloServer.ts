a119d446d382446afabd0447f151ba08433592af
Alexander Schau <alexander@schau.email>
2022-01-21 07:42:00 +0000

    migrate to latest svelte kit version

--- src/ApolloServer.ts
+++ src/ApolloServer.ts
@@ -1,28 +1,27 @@
 import type { GraphQLOptions, HttpQueryError } from "apollo-server-core";
 import { runHttpQuery } from "apollo-server-core";
-import type { ServerRequest } from "@sveltejs/kit/types/hooks";
-import type { EndpointOutput } from "@sveltejs/kit/types/endpoint";
+import type {RequestEvent} from "@sveltejs/kit/types/hooks"
 import { Headers, Request as ApolloRequest } from "apollo-server-env";
 import { ApolloServerBase } from "apollo-server-core";
+import type {RequestHandler} from "@sveltejs/kit"
 
 export class ApolloServer extends ApolloServerBase {
   async createGraphQLServerOptions(
-    req: ServerRequest
+    req: RequestEvent
   ): Promise<GraphQLOptions> {
     return await super.graphQLServerOptions({ req });
   }
 
-  public handleRequest = (
-    req: ServerRequest
-  ): Promise<EndpointOutput> | EndpointOutput => {
+  public handleRequest: RequestHandler = (req) => {
+
     new Headers({});
-    const acceptedTypes = (new Headers(req.headers).get("Accept") || "")
+    const acceptedTypes = (new Headers(req.request.headers).get("Accept") || "")
       .toLowerCase()
       .split(",");
     const landingPage = this.getLandingPage();
     if (
       landingPage &&
-      req.method === "GET" &&
+      req.request.method === "GET" &&
       acceptedTypes.includes("text/html")
     ) {
       return {
@@ -35,23 +34,23 @@ export class ApolloServer extends ApolloServerBase {
     }
 
     return runHttpQuery([req], {
-      method: req.method,
+      method: req.request.method,
       options: () => this.createGraphQLServerOptions(req),
       query:
-        req.method == "POST"
-          ? (req.body as any)
-          : req.query.get("query")
+      req.request.method == "POST"
+          ? (req.request.body as any)
+          : req.url.searchParams.get("query")
           ? {
-              query: req.query.get("query"),
-              variables: req.query.get("variables"),
-              operationName: req.query.get("operationName"),
-              extensions: req.query.get("extensions"),
+              query: req.url.searchParams.get("query"),
+              variables: req.url.searchParams.get("variables"),
+              operationName: req.url.searchParams.get("operationName"),
+              extensions: req.url.searchParams.get("extensions"),
             }
           : null,
       request: {
-        url: req.path,
-        headers: new Headers(req.headers),
-        method: req.method,
+        url: req.url.pathname,
+        headers: new Headers(req.request.headers),
+        method: req.request.method,
       } as ApolloRequest,
     }).then(
       ({ graphqlResponse, responseInit }) => {
